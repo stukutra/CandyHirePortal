26f3f0cc3cc373d2df017f50e37221a8e423409a
stukutra <stukutra@example.com>
2025-10-29 20:35:58 +0100

    feat: implement Guide component with template and styles; add initial unit tests

---
+++ src/app/pages/guide/guide.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-guide',
+  imports: [],
+  templateUrl: './guide.html',
+  styleUrl: './guide.scss',
+})
+export class Guide {
+
+}
